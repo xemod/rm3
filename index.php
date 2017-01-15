9693e22b4457b24a395f2fc1d117e09d2c2aa268
xemod <wichanan@gmail.com>
2017-01-15 21:04:55 +0700

    add rm3 project

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>โครงการพัฒนาผู้จัดการงานวิจัย รุ่นที่ ๓(Research Manager : RM)</title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css">
+
+<!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<meta name="viewport" content="width=device-width" />
+
+
+<script src="jquery-3.1.1.min.js" type="text/javascript"></script>
+<script src="modernizr-latest.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+<div id="wrapper">
+
+<!-- start html block -->
+<header>
+<hgroup id="hideh1"><h1 class="site-title"><a href="/rm3">อบรม RM รุ่นที่ ๓</a></h1></hgroup>
+<a href="#"><img src="https://www.hsri.or.th/sites/default/files/logo.png" onclick= "window.location.href = '/'" / id="headimg"></a>
+</header>
+
+<nav>
+<ul class="main-nav">
+<li><a href="/">หน้าแรก สวรส.</a></li>
+</ul>
+</nav>
+
+
+<section class="content">
+
+<!-- post 1 -->
+<article class="post">
+<h1 class="post-title"><a href="#">โครงการพัฒนาผู้จัดการงานวิจัย รุ่นที่ ๓(Research Manager : RM)</a></h1>
+
+<!-- adding post meta -->
+<div class="entry post-meta">
+	<span class="post-author">Webmaster</span>
+	<span class="post-date">15 January 2017</span>
+</div>
+
+<!-- adding post content -->
+<div class="entry post-content">
+<img src="https://www.hsri.or.th/sites/default/files/styles/front_slide__620x350_/public/Untitled.jpg" alt="" width="620" height="350" /><br >
+<p><u>การสมัครเข้ารับการฝึกอบรม</u></p>
+
+<p>ผู้ที่สนใจและได้รับอนุมัติจากหน่วยงานต้นสังกัดให้สมัครเข้ารับการฝึกอบรมได้ที่ <a href="http://www.hsri.or.th/rm3"><u>www.hsri.or.th</u><u>/</u><u>rm3</u></a> <u><strong>หมดเขตสมัคร</strong></u><u><strong>ภายใน</strong></u><u><strong>วันที่ 10</strong></u> <u><strong>กุมภาพันธ์ 2560</strong></u> และประกาศรายชื่อผู้มีสิทธิเข้ารับการอบรมในวันที่ 13 กุมภาพันธ์ 2560 ทางเว็บไซต์ <a href="http://www.hsri.or.th/rm3"><u>www.hsri.or.th</u><u>/</u><u>rm3</u></a></p>
+
+<p><span style="color:#FF0000">ทั้งนี้ ผู้เข้ารับการอบรมเบิกค่าใช้จ่าย ค่าเดินทาง ค่าที่พัก ค่าเบี้ยเลี้ยง จากต้นสังกัด</span></p></div>
+<p> ติดต่อผู้ประสานงาน : โทรศัพท์ 0-2832-9221 , 0-2832-9221 , 0-2832-9267-8 <br />
+<img src="email.png" alt="email to webmaster" style="vertical-align:middle" /> Email: hsri@hsri.or.th </p>
+</article>
+<!-- end post 1 -->
+
+
+</section>
+
+
+<aside class="sidebar">
+<ul class="widget-sidebar">
+
+<!-- some sample list -->
+<li class="widget widget_categories">
+<h3 class="widget-title">ดาวน์โหลดไฟล์</h3>
+<ul>
+<li><img src="pdf_icon.gif" /> <a href="project.pdf">โครงการ</a></li>
+<li><img src="pdf_icon.gif" /> <a href="sche.pdf">กำหนดการ</a></li>
+
+
+<!-- some sample text block -->
+<li class="widget widget_text">
+<h3 class="widget-title">สมัครเข้าอบรม</h3>
+<div class="textwidget">
+<p>
+	สมัครเข้าอบรมหลักสูตรพัฒนาผู้จัดการงานวิจัย รุ่นที่ ๓(Research Manager) กรุณากรอกแบบฟอร์มออนไลน์ ได้ที่ปุ่มด้านล่าง
+</p>
+	<a href="https://goo.gl/forms/5VvcLyKAaV8vIeVA3" class="btn" title="แบบฟอร์มสมัครเข้าอบรมออนไลน์" target="_blank">สมัครเข้าอบรม</a>
+<p>
+	<strong>หมายเหตุ</strong> สามารถรับได้ 100 ท่าน ทั้งนี้ ท่านที่สมัครก่อนจะได้สิทธิในการเลือกวันเข้ารับการอบรมก่อน  ขอสงวนสิทธิ์ให้ผู้ที่ยังไม่เคยสมัครอบรมในรุ่นที่ 1-2
+</p>
+
+</div>
+</li>
+
+
+</ul>
+</aside>
+
+
+<footer>
+<div class="footer-left">Copyright@ 2017 <a href="/">hsri.or.th</a></div>
+<div class="footer-right"><a href="/researcher/about/history">About Us</a></div>
+</footer>
+
+<!-- end html block -->
+
+</div>
+</body>
+
+
+</html>
